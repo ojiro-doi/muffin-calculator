680760148c0de20e66badbd6866caf35ca920089
Ojiro <131632001+ojiro-doi@users.noreply.github.com>
2024-01-27 17:25:23 +0900

    ボタン追加 (#1)
    * btn計算機能実装
    
    * ボタン追加
    
    * ボタン追加２
    
    ---------
    
    Co-authored-by: “ojiro-doi” <sakura.0046@icoud.com>

--- cainz/app.js
+++ cainz/app.js
@@ -1,1 +1,65 @@
-const x=
+const btn = 
+    {
+        price: [100, 110, 120, 130],
+        name: ["プレーン", "チョコ", "抹茶", "イチゴ"]
+    };
+
+
+let total = 0;
+let number = 0;
+const btnlength=btn.name.length;
+const $button = document.getElementsByClassName("btn");
+const $buttonName=$button.querySelect
+const $numberdisplay = document.getElementById("number-dis");
+const $totaldisplay = document.getElementById("total-dis");
+
+// class Button{
+//     constructor(price,name){
+//         this.price=price;
+//         this.name=name;
+//     }
+// }
+
+
+
+//クリック時のpriceの合計、回数を表示
+const ClickButton = (e) => {
+    for (let i = 0; i < btnlength; i++) {
+        if (btn.name[i] === e.target.textContent) {
+            total += btn.price[i];
+            number++;
+        }
+    }
+
+    console.log(e.target);
+    Display();
+}
+
+
+// ボタン
+const Button_function = () => {
+    for (let i = 0; i < btnlength; i++) {
+        // ボタン名前
+        $button[i].textContent = btn.name[i];
+    }
+
+
+    for (let j = 0; j < btnlength; j++) {
+        // ボタンclick
+        $button[j].addEventListener("click", (e) => {
+            ClickButton(e);
+            console.log(total);
+            console.log(number);
+        });
+    }
+}
+
+
+//画面表示
+const Display = () => {
+    $numberdisplay.textContent = number;
+    $totaldisplay.textContent = total;
+}
+
+Button_function();
+Display();
