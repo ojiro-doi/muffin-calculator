c2f779c45039c1b80de32ab1e72998da41361977
“ojiro-doi” <sakura.0046@icoud.com>
2024-02-03 20:09:23 +0900

    undoの実装

--- cainz/app.js
+++ cainz/app.js
@@ -14,7 +14,6 @@ const btn = [
 
 ]
 
-
 let total = 0;
 let number = 0;
 let total_pre = 0;
@@ -23,38 +22,8 @@ const btnLength = btn.length;
 const $buttonName = document.getElementsByClassName("btn-name");
 const $button = document.getElementsByTagName("button");
 const $buttonImage = document.getElementsByClassName("Image");
-
 const $numberdisplay = document.getElementById("number-dis");
 const $totaldisplay = document.getElementById("total-dis");
-
-// class Button{
-//     constructor(price,name){
-//         this.price=price;
-//         this.name=name;
-//     }
-// }
-
-
-
-// クリック時のpriceの合計、回数を表示
-// const ClickButton = (e) => {
-//     for (let i = 0; i < btnLength; i++) {
-//         if (btn.name[i] === e.target.textContent) {
-//             total += btn.price[i];
-//             number++;
-//         }
-//     }
-
-//     console.log(e.target);
-//     display();
-// }
-
-
-// const clickedElement = e.target;
-// ClickButton();
-// console.log(total);
-// console.log(number);
-
 
 //ボタン
 const buttonName = () => {
@@ -62,44 +31,41 @@ const buttonName = () => {
         // ボタン名前
         $buttonName[i].textContent = btn[i].name;
     }
-    // $button.addEventListener("click", (e) => {
-    //     ClickButton(e);
-    //     console.log(total);
-    //     console.log(number);
-    // });
-
-
-    // for (let j = 0; j < btnLength; j++) {
-    //     // ボタンclick
-    //     $button[j].addEventListener("click", (e) => {
-    //         ClickButton(e);
-    //         console.log(total);
-    //         console.log(number);
-    //     });
-    // }
 };
 
 //画面表示
 const display = () => {
     $numberdisplay.textContent = number;
     $totaldisplay.textContent = total;
-}
+};
 
 const displayLog = () => {
-    console.log(`回数：${number}`);
-    console.log(`合計：${total}`);
-}
+    if (number % 3 === 0 && number !== 0) {
+        console.log("totalから-30");
+    }
+    console.log(`number : ${number}`);
+    console.log(`total  : ${total}`);
+    console.log("");
+};
 
+const displayColor = () => {
+    if (number % 3 === 0 && number !== 0) {
+        $numberdisplay.classList.add('highlighted_3');
+    } else {
+        $numberdisplay.classList.remove('highlighted_3');
+    }
+    if (number === 6 && total > 880 || number === 9 && total > 1260) {
+        console.log("セットの方が安い");
+        $totaldisplay.classList.add('highlighted');
+    } else {
+        $totaldisplay.classList.remove('highlighted');
+        console.log("単品の方が安い")
+    }
+};
+
+//初期表示
 buttonName();
 display();
-
-// const Click = () => {
-//     $button.addEventListener("click", (e) => {
-//         ClickButton(e);
-//         console.log(total);
-//         console.log(number);
-//     });
-// };
 
 class click {
     constructor(buttonId, buttonNumber) {
@@ -108,33 +74,17 @@ class click {
     }
 
     clickFunction() {
+        //計算
         if (this.buttonId === `btn${this.buttonNumber}`) {
             console.log("clickインスタンスが作成されました");
             total += btn[this.buttonNumber].price;
             number++;
-            if (number % 3 === 0) {
-                console.log("totalで-30")
+            if (number % 3 === 0 && number !== 0) {
                 total -= 30;
-                $numberdisplay.classList.add('highlighted_3');
-            } else {
-                $numberdisplay.classList.remove('highlighted_3');
-            }
-            if (number === 6 && total > 880 || number === 9 && total > 1260) {
-                console.log("セットの方が安い")
-                $totaldisplay.classList.add('highlighted');
-            } else {
-                $totaldisplay.classList.remove('highlighted');
             }
         }
-        // 画面表示更新
-        display();
-
-
     }
-}
-
-
-
+};
 
 const buttonClick = (buttonId) => {
     //クリック前のvalueを保存
@@ -144,29 +94,30 @@ const buttonClick = (buttonId) => {
         const clickInstance = new click(buttonId, i);
         clickInstance.clickFunction();
     }
-    // const click0 = new click(buttonId, 0);
-    // click0.clickFunction();
-    // const click1 = new click(buttonId, 1);
-    // click1.clickFunction();
-    // const click2 = new click(buttonId, 2);
-    // click2.clickFunction();
 
+    // 画面表示更新
+    display();
+    displayColor();
     displayLog();
-}
+};
 
 const resetValue = () => {
     total = 0;
     number = 0;
+    console.log("resetされました");
     display();
     displayLog();
-}
+    displayColor();
+};
 
 const undoValue = () => {
-    total=total_pre;
-    number=number_pre;
+    total = total_pre;
+    number = number_pre;
+    console.log("一つ前に戻りました");
     display();
     displayLog();
-}
+    displayColor();
+};
 
 
 
